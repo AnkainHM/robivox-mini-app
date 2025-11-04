253a7706fff5ad28fb489f332bf9769ae5333763
TarasSopelnichenko <taras.sopelnichenko@42crunch.com>
2025-11-05 00:32:08 +0700

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/robivox-mini-app/',
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  base: '/',
 });
